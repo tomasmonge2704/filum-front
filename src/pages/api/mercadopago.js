6d5d24dc886944450430289f07f4f211f831ea57
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2023-05-21 14:44:55 -0300

    fix

--- src/pages/api/mercadopago.js
+++ src/pages/api/mercadopago.js
@@ -3,34 +3,31 @@ const mpaccesstoken = process.env.mpaccesstoken;
 import axios from "axios";
 
 export default async function handler(req, res) {
-    if(req.body.data && req.body.type == 'payment'){
+  if (req.body.data && req.body.type === 'payment') {
     const compra = await axios.get(
-        `https://api.mercadopago.com/v1/payments/${req.body.data.id}`,
-        {
-          headers: {
-            "Content-type": "application/json",
-            Authorization: `Bearer ${mpaccesstoken}`,
-          },
-        }
-      );
-      console.log(compra)
-      async () => {
-        try {
-          const body = compra.data.metada;
-          const response = await fetch(`${API_URL}/api/compras`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authentication: `${compra.data.metadata.token}`,
-            },
-            body,
-          });
-          console.log(response)
-        } catch (error) {
-          console.log(error);
-        }
-      };
+      `https://api.mercadopago.com/v1/payments/${req.body.data.id}`,
+      {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${mpaccesstoken}`,
+        },
+      }
+    );
+
+    try {
+      const body = compra.data.metadata;
+      const response = await axios.post(`${API_URL}/api/compras`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authentication: `${compra.data.metadata.token}`,
+        },
+        body
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
     }
-   
-    res.status(200).json({ name: 'tomas Doe' })
   }
+
+  res.status(200).json({ name: 'tomas Doe' });
+}
