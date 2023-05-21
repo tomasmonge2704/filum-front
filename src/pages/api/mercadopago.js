00fbbb72a93da207d17fd43c92ca44aa21d1311f
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2023-05-21 03:45:24 -0300

    fix

--- src/pages/api/mercadopago.js
+++ src/pages/api/mercadopago.js
@@ -1,12 +1,12 @@
-import Router from "next/router";
 const API_URL = process.env.NEXT_PUBLIC_API_KEY;
 import { UseCompraContext } from "@/utils/compraContextUtil";
 export default function handler(req, res) {
     const {compra,setCompra} = UseCompraContext();
-    console.log(compra)
-    async function postCompra() {
+    console.log(req)
+    const postCompra = async () => {
         try {
-          const response = await fetch(`${API_URL}/api/compras`, {
+          const body = await compra;
+          await fetch(`${API_URL}/api/compras`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
@@ -14,12 +14,13 @@ export default function handler(req, res) {
             },
             body,
           });
-    
-          const data = await response.json();
-          Router.push(`/mis-compras/${data._id}`);
         } catch (error) {
           alert(error);
         }
+      };
+    
+      if (compra.length !== 0) {
+        postCompra();
       }
     res.status(200).json({ name: 'tomas Doe' })
   }
